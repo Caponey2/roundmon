2760861c4e3f03eab271be283a7d2fa14b22f22f
tannerhebert <tannerhebert20xx@gmail.com>
2022-08-15 16:27:48 -0700

    Tailwind fixed and voting boxes centered

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import { trpc } from '../utils/trpc';
 import 'tailwindcss/tailwind.css';
 
-type TechnologyCardProps = {
-	name: string;
-	description: string;
-	documentation: string;
-};
-
-const Home: NextPage = () => {
+export default function Home() {
 	//const hello = trpc.useQuery(['example.hello', { text: 'from tRPC' }]);
 
-	return <div className='text-2xl text-red-300'>Hello World</div>;
-};
-
-export default Home;
+	return (
+		<div className='h-screen w-screen flex flex-col justify-center items-center'>
+			<div className='text-2xl text-center'>Which Pokemon is rounder?</div>
+			<div className='p-2' />
+			<div className='border rounded p-8 flex justify-between items-center max-w-2xl'>
+				<div className='w-16 h-16 bg-red-200' />
+				<div className='p-8'>Vs</div>
+				<div className='w-16 h-16 bg-red-200' />
+			</div>
+		</div>
+	);
+}
